eb939a44a26bc9ae389b18e83164bce99f1bc0f9
AlfieTerryyy <168709104+AlfieTerryyy@users.noreply.github.com>
2024-07-02 10:49:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+    function toggleNav() {
+      const navButtons = document.querySelectorAll('.nav-square');
+      navButtons.forEach(button => {
+        button.classList.toggle('hidden'); // Add a CSS class to hide/show the buttons
+      });
+    }
